ea950bf7e49e8d906e4801e99aac502e5d779c58
siddharth-1118 <saisiddharthvooka@gmail.com>
2025-12-07 21:06:11 +0530

    Added NextAuth tables

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   providers: [
